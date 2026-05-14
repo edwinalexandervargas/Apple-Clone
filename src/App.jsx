11018377cf1a7d948135e0f05c46ed6b3461cceb
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-13 22:04:52 -0700

    Fix merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import Hero  from './components/Hero';
-import ProductViewer from './components/ProductViewer';
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger)
 
--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -1,10 +1,10 @@
-import { PresentationControls } from '@react-three/drei';
-import React, { useRef } from 'react'
-import MacbookModel16 from '../models/Macbook-16';
-import MacbookModel14 from '../models/Macbook-14';
+import { useRef} from "react";
+import {PresentationControls} from "@react-three/drei";
 import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
 
+import MacbookModel16 from "../models/Macbook-16.jsx";
+import MacbookModel14 from "../models/Macbook-14.jsx";
+import {useGSAP} from "@gsap/react";
 const ANIMATION_DURATION = 1;
 const OFFSET_DISTANCE = 5;
 
@@ -14,7 +14,7 @@ const fadeMeshes = (group, opacity) => {
     group.traverse((child) => {
         if(child.isMesh) {
             child.material.transparent = true;
-            gsap.to(child.material, {opacity, duration: ANIMATION_DURATION })
+            gsap.to(child.material, { opacity, duration: ANIMATION_DURATION })
         }
     })
 }
@@ -31,20 +31,20 @@ const ModelSwitcher = ({ scale, isMobile }) => {
 
     const smallMacbookRef = useRef();
     const largeMacbookRef = useRef();
-    
+
     const showLargeMacbook = scale === SCALE_LARGE_DESKTOP || scale === SCALE_LARGE_MOBILE;
 
     useGSAP(() => {
         if(showLargeMacbook) {
-            moveGroup(smallMacbookRef.current, - OFFSET_DISTANCE)
+            moveGroup(smallMacbookRef.current, -OFFSET_DISTANCE);
             moveGroup(largeMacbookRef.current, 0);
-    
+
             fadeMeshes(smallMacbookRef.current, 0);
             fadeMeshes(largeMacbookRef.current, 1);
         } else {
             moveGroup(smallMacbookRef.current, 0);
             moveGroup(largeMacbookRef.current, OFFSET_DISTANCE);
-    
+
             fadeMeshes(smallMacbookRef.current, 1);
             fadeMeshes(largeMacbookRef.current, 0);
         }
@@ -55,23 +55,23 @@ const ModelSwitcher = ({ scale, isMobile }) => {
         speed: 1,
         zoom: 1,
         azimuth: [-Infinity, Infinity],
-        config: {mass: 1, tension: 0, friction: 26}
+        config: {mass:1, tension: 0, friction: 26}
     }
-  return (
-    <>
-        <PresentationControls {...controlsConfig}>
-            <group ref={largeMacbookRef}>
-                <MacbookModel16 scale= {isMobile ? 0.05 : 0.08} />
-            </group>
-        </PresentationControls>
 
-        <PresentationControls {...controlsConfig}>
-            <group ref={smallMacbookRef} position={[-OFFSET_DISTANCE, 0, 0]}>
-                <MacbookModel14 scale= {isMobile ? 0.03 : 0.06} />
-            </group>
-        </PresentationControls>
-    </>
-  )
+    return (
+        <>
+            <PresentationControls {...controlsConfig}>
+                <group ref={largeMacbookRef}>
+                    <MacbookModel16 scale={isMobile ? 0.05 : 0.08} />
+                </group>
+            </PresentationControls>
+
+            <PresentationControls {...controlsConfig}>
+                <group ref={smallMacbookRef}>
+                    <MacbookModel14 scale={isMobile ? 0.03 : 0.06} />
+                </group>
+            </PresentationControls>
+        </>
+    )
 }
-
 export default ModelSwitcher
