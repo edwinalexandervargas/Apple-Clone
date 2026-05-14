8ac23aecf526418424447b67a6c11e0a1edeade7
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-13 21:59:43 -0700

    Fix Showcase section and GSAP registration

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
 import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from 'gsap/all';
 import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
 
 gsap.registerPlugin(ScrollTrigger)
@@ -15,6 +18,11 @@ const App = () => {
     <NavBar />
     <Hero />
     <ProductViewer />
+    <Showcase />
+    <Performance />
+    <Features />
+    <Highlights />
+    <Footer />
   </main>
 
     )
